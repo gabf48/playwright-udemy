e72230eed45ae5137fe31daf12029c80e7d3d40d
Gabriel Filip <gabf48@gmail.com>
2026-01-13 17:28:37 +0200

    locators & type into elements

--- tests/UIBasictest.spec.js
+++ tests/UIBasictest.spec.js
@@ -1,15 +1,18 @@
 const {test, expect} = require('@playwright/test');
 
-test('Browser Context Playwright test', async ({browser})=>
+test.only('Browser Context Playwright test', async ({browser})=>
     {
         // chrome - plugin/ coockies
             const context = await browser.newContext();
             const page = await context.newPage();
             await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
-    }); 
+            await page.locator('#username').fill("rahulshetty")
+            await page.locator('#password').fill("learning")
+            await page.locator('#signInBtn').click()
+        }); 
 
 
-test.only('Page Playwright test', async ({page})=>
+test('Page Playwright test', async ({page})=>
 {
     await page.goto("https://www.google.com");
     console.log(await page.title());
