d9615541a9a3f25f26e096f360a310b70b8b7ae3
Gabriel Filip <gabf48@gmail.com>
2026-01-13 11:07:07 +0200

    configuration file and its details to run the tests

--- tests/UIBasictest.spec.js
+++ tests/UIBasictest.spec.js
@@ -2,14 +2,14 @@ const {test} = require('@playwright/test');
 
 test('Browser Context Playwright test', async ({browser})=>
     {
-        chrome - plugin/ coockies
+        // chrome - plugin/ coockies
             const context = await browser.newContext();
             const page = await context.newPage();
-            await page.goto("www.google.com");   
+            await page.goto("https://www.google.com");
     }); 
 
 
-test('Page Playwright test', async ({page})=>
+test.only('Page Playwright test', async ({page})=>
 {
-    await page.goto("www.google.com");   
+    await page.goto("https://www.google.com");
 }); 
