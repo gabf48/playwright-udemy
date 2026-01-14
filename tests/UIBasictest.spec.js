87b5e851ac639f7b46bc0b401efe061dcf5d7999
Gabriel Filip <gabf48@gmail.com>
2026-01-14 10:55:01 +0200

    how to work with locators which extract multiple webelements in page

--- tests/UIBasictest.spec.js
+++ tests/UIBasictest.spec.js
@@ -2,16 +2,27 @@ const {test, expect} = require('@playwright/test');
 
 test.only('Browser Context Playwright test', async ({browser})=>
     {
-        // chrome - plugin/ coockies
-            const context = await browser.newContext();
-            const page = await context.newPage();
-            await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
-            await page.locator('#username').fill("rahulshetty")
-            await page.locator('#password').fill("learning")
-            await page.locator('#signInBtn').click()
-            // wait until this locator shown up page
-            console.log(await page.locator("[style*='block']").textContent());
-            await expect(page.locator("[style*='block']")).toContainText('Incorrect username/password.');
+
+        const context = await browser.newContext();
+        const page = await context.newPage();
+        
+        const userName = page.locator('#username');
+        const signIn = page.locator('#signInBtn');
+        const password = page.locator('#password');
+        
+        await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+        await userName.fill("rahulshetty")
+        await password.fill("learning")
+        await signIn.click()
+        // wait until this locator shown up page
+        console.log(await page.locator("[style*='block']").textContent());
+        await expect(page.locator("[style*='block']")).toContainText('Incorrect username/password.');
+
+        await userName.fill("");
+        await userName.fill("rahulshettyacademy");
+        await signIn.click()
+        console.log(await page.locator(".card-body a").first().textContent()); 
+        console.log(await page.locator(".card-body a").nth(1).textContent()); 
         }); 
 
 
