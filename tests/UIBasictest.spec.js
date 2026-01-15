fe124ad72be036042f92239485cf1cfeec5667d1
Gabriel Filip <gabf48@gmail.com>
2026-01-15 14:48:04 +0200

    Handing Child windows & Tabs with Playwright by switching browser context

--- tests/UIBasictest.spec.js
+++ tests/UIBasictest.spec.js
@@ -1,6 +1,6 @@
 const {test, expect} = require('@playwright/test');
 
-test.only('Browser Context Playwright test', async ({browser})=>
+test('Browser Context Playwright test', async ({browser})=>
     {
 
         const context = await browser.newContext();
@@ -50,3 +50,25 @@ test('UI Controls', async ({page})=>
     // assertion
     await page.pause();
 }); 
+
+
+test('@Child windows hadl', async ({browser})=>
+{
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    const userName = page.locator('#username');
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    const documentLink = page.locator("[href*='documents-request']");
+    
+    const [newPage] = await Promise.all([
+        context.waitForEvent('page'), //listen for any new page pending, rejected,fulfilled
+        documentLink.click(),
+    ]) 
+    const text = await newPage.locator(".red").textContent();
+    const arrayText = text.split("@")
+    const domain = arrayText[1].split(" ")[0]
+    console.log(domain);
+    await page.locator("#username").type(domain);
+    await page.pause();
+    console.log(await page.locator("#username").textContent());
+});
