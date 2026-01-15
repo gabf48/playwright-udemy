1ec84e5779badf0fc5e533fc6dda6c46dcc7db65
Gabriel Filip <gabf48@gmail.com>
2026-01-15 18:54:09 +0200

    Add assertions for the actions perfromed and implement necessary Sync steps

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -2,27 +2,28 @@ const {test, expect} = require('@playwright/test');
 
 test.only('Browser Context Playwright test', async ({page})=>
     {
-
-        const productName = 'Zara Coat 4';
+        const email = "anunturi.user@gmail.com";
+        const productName = 'ZARA COAT 3';
         const products = page.locator(".card-body");
         await page.goto("https://rahulshettyacademy.com/client");
-        await page.locator("#userEmail").fill("anshika@gmail.com");
-        await page.locator("#userPassword").fill("Iamking@000");
+        await page.locator("#userEmail").fill(email);
+        await page.locator("#userPassword").fill("Parola1993!");
         await page.locator("[value='Login']").click();
-        await page.waitForLoadState('networkidle')
-        // await page.locator(".card-body b").first().waitFor();
+        await page.waitForLoadState('networkidle');
+        await page.locator(".card-body b").first().waitFor();
         const titles = await page.locator(".card-body b").allTextContents();
         console.log(titles);
         const count = await products.count();
         for(let i=0; i<=count; i++)
         {
-            if(await products.nth(i).locator("b").textContent() == productName){
-                await productName.nth(i).locator("text=Add To Card").click();
+            if(await products.nth(i).locator("b").textContent() === productName){
+                await products.nth(i).locator("text= Add To Cart").click();
                 break;
             }
         }
 
-
-        // Zara Coat 4
-
+        await page.locator("[routerlink*='cart']").click();
+        await page.locator("div li").first().waitFor();
+        const bool = page.locator("h3:has-text('Zara Coat 4')").isVisible();
+        expect(bool).toBeTruthy();
         }); 
