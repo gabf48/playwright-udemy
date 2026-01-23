5d32bd8c0aafd59920402d4a68af1137fea8b278
Gabriel Filip <gabf48@gmail.com>
2026-01-23 12:59:16 +0200

    Caputre Screenshot with Playwright on page & partial Element level

--- tests/MoreValidations.spec.js
+++ tests/MoreValidations.spec.js
@@ -20,3 +20,14 @@ test("Popup validations", async({page})=>
     console.log(textCheck.split(" ")[1]);
 
 })
+
+test.only("Screenshot & Visual comparision", async({page})=>
+{
+    await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
+    await expect(page.locator("#displayed-text")).toBeVisible();
+    await page.locator("#displayed-text").screenshot({path: "partialScreenshot.png"})
+    await page.locator("#hide-textbox").click();
+    await page.screenshot({path: 'screenshot.png'})
+    await expect(page.locator("#displayed-text")).toBeHidden();
+}
+)
