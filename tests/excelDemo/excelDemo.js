74dafed58a891177ef4630349113c6df9cdb76f1
Gabriel Filip <gabf48@gmail.com>
2026-01-26 10:16:52 +0200

    Build Util function to read and update excel file strategically

--- tests/excelDemo/excelDemo.js
+++ tests/excelDemo/excelDemo.js
@@ -1,6 +1,8 @@
 const ExcelJs = require("exceljs");
 
 async function excelTest() {
+    let output = {row:-1,column:-1};
+
   const Workbook = new ExcelJs.Workbook();
   await Workbook.xlsx.readFile(
     "F:\\playwright-udemy\\tests\\excelDemo\\exceldownload.xlsx"
@@ -8,9 +10,18 @@ async function excelTest() {
   const worksheet = Workbook.getWorksheet(1);
   worksheet.eachRow((row, rowNumber) => {
     row.eachCell((cell, colNumber) => {
-      console.log(cell.value);
+      if (cell.value === "Banana") {
+        output.row = rowNumber;
+        output.column = colNumber;
+      }
     });
   });
+
+  const cell = worksheet.getCell(output.row, output.column);
+  cell.value = "Republic";
+  await Workbook.xlsx.writeFile(
+    "F:\\playwright-udemy\\tests\\excelDemo\\exceldownload.xlsx"
+  );
 }
 
 excelTest();
