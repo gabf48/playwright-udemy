e6283f81124ef74159e2fd61c5251ae73b0387cb
Gabriel Filip <gabf48@gmail.com>
2026-01-26 18:14:57 +0200

    What is page object pattern & Importance of its implmentation

---
+++ tests/ClientAppPO.spec.js
@@ -0,0 +1,65 @@
+const {test, expect} = require('@playwright/test');
+const {LoginPage} = require('../pageobjects/LoginPage');
+
+test('Client App login', async ({page})=>
+    {
+        const username = "anunturi.user@gmail.com";
+        const password = "Parola1993!";
+        const productName = 'ZARA COAT 3';
+        const products = page.locator(".card-body");
+
+        const loginPage = new LoginPage(page);
+        loginPage.goTo();
+        loginPage.validLogin(username, password);
+       
+        await page.waitForLoadState('networkidle');
+        await page.locator(".card-body b").first().waitFor();
+        const titles = await page.locator(".card-body b").allTextContents();
+        console.log(titles);
+        const count = await products.count();
+        for(let i=0; i<=count; i++)
+        {
+            if(await products.nth(i).locator("b").textContent() === productName){
+                await products.nth(i).locator("text= Add To Cart").click();
+                break;
+            }
+        }
+
+        await page.locator("[routerlink*='cart']").click();
+        await page.locator("div li").first().waitFor();
+        const bool = page.locator("h3:has-text('Zara Coat 4')").isVisible();
+        expect(bool).toBeTruthy();
+        await page.locator("text=Checkout").click();
+        await page.locator("[placeholder*='Country']").pressSequentially('rom');
+        const dropdown = page.locator(".ta-results");
+        await dropdown.waitFor();
+        const optionsCount = await dropdown.locator("button").count();
+        for(let i=0; i< optionsCount; i++){
+            const text = await dropdown.locator("button").nth(i).textContent();
+            if(text === " Romania"){
+                await dropdown.locator("button").nth(i).click();
+                break;
+            }
+        }
+
+        await expect(page.locator(".user__name [type='text']").first()).toHaveText(username);
+        await page.locator(".action__submit").click();
+        await expect(page.locator(".hero-primary")).toHaveText(" Thankyou for the order. ");
+        const orderId = await page.locator(".em-spacer-1 .ng-star-inserted").textContent();
+        console.log(orderId);
+        await page.locator("button[routerlink*='myorders']").click();
+        await page.locator("tbody").waitFor();
+        const rows = await page.locator("tbody tr");
+        
+        for (let i =0; i< await rows.count(); i++)
+        {
+            const rowOrderId = await rows.nth(i).locator("th").textContent();
+            if (orderId.includes(rowOrderId))
+            {
+                await rows.nth(i).locator("button").first().click();
+                break;
+            }
+        }
+        const orderIdDetails = await page.locator(".col-text").textContent();
+        expect(orderId.includes(orderIdDetails)).toBeTruthy();
+        }); 
