1b81d939c45f227c6c81f5ecc6ca142d26f24602
Gabriel Filip <gabf48@gmail.com>
2026-01-27 19:26:25 +0200

    Assignment Solution - Converting Page object Model Project

--- pageobjects/POManager.js
+++ pageobjects/POManager.js
@@ -1,20 +1,38 @@
-const {LoginPage} = require('./LoginPage');
-const {DashboardPage} = require('./DashboardPage');
+const { LoginPage } = require("./LoginPage");
+const { DashboardPage } = require("./DashboardPage");
+const { CartPage } = require("./CartPage");
+const { OrdersReviewPage } = require("./OrdersReviewPage");
+const { OrdersHistoryPage } = require("./OrdersHistoryPage"); 
 
-class POManager{
-    constructor(page){
-        this.page = page;
-        this.loginPage = new LoginPage(this.page);
-        this.dashboardPage = new DashboardPage(this.page);
-    }
+class POManager {
+  constructor(page) {
+    this.page = page;
+    this.loginPage = new LoginPage(this.page);
+    this.dashboardPage = new DashboardPage(this.page);
+    this.cartPage = new CartPage(this.page);
+    this.ordersHistoryPage = new OrdersHistoryPage(this.page);
+    this.ordersReviewPage = new OrdersReviewPage(this.page)
+  }
 
-getLoginPage(){
+  getLoginPage() {
     return this.loginPage;
-}
+  }
 
-getDashboardPage(){
+  getDashboardPage() {
     return this.dashboardPage;
-}
+  }
+
+  getCartPage() {
+    return this.cartPage;
+  }
+
+  getOrdersReviewPage(){
+    return this.ordersReviewPage;
+  }
+
+  getOrdersHistoryPage(){
+    return this.ordersHistoryPage;
+  }
 }
 
-module.exports = {POManager};
+module.exports = { POManager };
--- tests/ClientAppPO.spec.js
+++ tests/ClientAppPO.spec.js
@@ -1,55 +1,30 @@
-const {test, expect} = require('@playwright/test');
-const {POManager} = require('../pageobjects/POManager');
+const { test, expect } = require("@playwright/test");
+const { POManager } = require("../pageobjects/POManager");
 
-test('Client App login', async ({page})=>
-    {
-        const poManager = new POManager(page);
-        const username = "anunturi.user@gmail.com";
-        const password = "Parola1993!";
-        const productName = 'ZARA COAT 3';
-        const products = page.locator(".card-body");
+test("Client App login", async ({ page }) => {
+  const poManager = new POManager(page);
+  const username = "anunturi.user@gmail.com";
+  const password = "Parola1993!";
+  const productName = "ZARA COAT 3";
+  const products = page.locator(".card-body");
 
-        const loginPage = poManager.getLoginPage();
-        const dashboardPage = poManager.getDashboardPage();
-        await loginPage.goTo();
-        await loginPage.validLogin(username, password);
-        await dashboardPage.searchProductAddCart(productName);
-        await dashboardPage.navigateToCart();
+  const loginPage = poManager.getLoginPage();
+  await loginPage.goTo();
+  await loginPage.validLogin(username, password);
+  const dashboardPage = poManager.getDashboardPage();
+  await dashboardPage.searchProductAddCart(productName);
+  await dashboardPage.navigateToCart();
 
-        await page.locator("div li").first().waitFor();
-        const bool = page.locator("h3:has-text('Zara Coat 4')").isVisible();
-        expect(bool).toBeTruthy();
-        await page.locator("text=Checkout").click();
-        await page.locator("[placeholder*='Country']").pressSequentially('rom');
-        const dropdown = page.locator(".ta-results");
-        await dropdown.waitFor();
-        const optionsCount = await dropdown.locator("button").count();
-        for(let i=0; i< optionsCount; i++){
-            const text = await dropdown.locator("button").nth(i).textContent();
-            if(text === " Romania"){
-                await dropdown.locator("button").nth(i).click();
-                break;
-            }
-        }
+  const cartPage = poManager.getCartPage();
+  await cartPage.VerifyProductIsDisplayed(productName);
+  await cartPage.Checkout();
 
-        await expect(page.locator(".user__name [type='text']").first()).toHaveText(username);
-        await page.locator(".action__submit").click();
-        await expect(page.locator(".hero-primary")).toHaveText(" Thankyou for the order. ");
-        const orderId = await page.locator(".em-spacer-1 .ng-star-inserted").textContent();
-        console.log(orderId);
-        await page.locator("button[routerlink*='myorders']").click();
-        await page.locator("tbody").waitFor();
-        const rows = await page.locator("tbody tr");
-        
-        for (let i =0; i< await rows.count(); i++)
-        {
-            const rowOrderId = await rows.nth(i).locator("th").textContent();
-            if (orderId.includes(rowOrderId))
-            {
-                await rows.nth(i).locator("button").first().click();
-                break;
-            }
-        }
-        const orderIdDetails = await page.locator(".col-text").textContent();
-        expect(orderId.includes(orderIdDetails)).toBeTruthy();
-        }); 
+  const ordersReviewPage = poManager.getOrdersReviewPage();
+  await ordersReviewPage.searchCountryAndSelect("romania", "Romania");
+  const orderId = await ordersReviewPage.SubmitAndGetOrderId();
+  console.log(orderId);
+  await dashboardPage.navigateToOrders();
+  const ordersHistoryPage = poManager.getOrdersHistoryPage();
+  await ordersHistoryPage.searchOrderAndSelect(orderId);
+  expect(orderId.includes(await ordersHistoryPage.getOrderId())).toBeTruthy();
+});
