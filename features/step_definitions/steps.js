9971d26fd82077474609ae202eb2c0886fb9cdcf
Gabriel Filip <gabf48@gmail.com>
2026-01-29 15:17:44 +0200

    Create Steps Definition files and associate with Cucumber.js config file

---
+++ features/step_definitions/steps.js
@@ -0,0 +1,23 @@
+const {When, Then, Given } = require('@cucumber/cucumber')
+
+
+Given('a login to Ecommerce application with {username} and {password}', function (username, password) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+
+  When('Add {string} to Cart', function (string) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  Then('Verify {string} is display in the Cart', function (string) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  When('Enter valid details and Place the Order', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
